934b1b71abb81d544bea6b12d7dbadee27385e6d
Thamizhselvan T <84961349+ThamizhselvanT@users.noreply.github.com>
2021-05-30 12:11:35 +0530

    Add files via upload

---
+++ SGPA CALCULATOR/database.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost= "sql107.epizy.com:3306";
+          $dbname= "epiz_27500361_cgpa";
+          $dbuser= "epiz_27500361";
+          $dbpass= "mQ49023b73U3";
+          $conn= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+ ?>
